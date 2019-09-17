31e00407e2777545220467debbd4dc82c56a32d4
yogendra saxena <yogendrasaxena@Admins-MacBook-Pro-4.local>
2019-09-17 14:35:57 +0530

    string algo modified

--- string-reversal.js
+++ string-reversal.js
@@ -1,3 +1,10 @@
 let str = "yogendra";
 let result = str.split("").reverse().join("");
 console.log(result);
+
+//---------other way---------
+
+let name = "yogendra";
+let res = name.split("").reduceRight((item, acc)=>item+acc);
+//let res = name.split("").reduce((item, acc)=>acc+item);
+console.log(res);
