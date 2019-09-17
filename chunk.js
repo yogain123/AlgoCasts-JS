cece6fbe65154fefc4f3c61625e605caa9003ebc
yogendra saxena <yogendrasaxena@Admins-MacBook-Pro-4.local>
2019-09-17 13:15:11 +0530

    Added Algo for Max-char and Array Chunking

---
+++ chunk.js
@@ -0,0 +1,10 @@
+console.log(chunk([1,2,3,4,5],4));
+function chunk(arr, size) {
+    let result = [];
+    let index = 0;
+    while(index<arr.length){
+        result.push(arr.slice(index, index + size));
+        index = index + size;
+    }
+    return result;
+}
---
+++ max-char.js
@@ -0,0 +1,15 @@
+let str = "qwwqwqqqqqqqqaaaaaaaaaaaaaaa";
+let obj = {};
+for(let i of str)
+    obj[i] = obj[i] + 1 || 1;
+
+let maxCount = 0;
+let maxLetter = "";
+for(let [key, value] of Object.entries(obj)){
+    if(value>=maxCount){
+        maxCount = value;
+        maxLetter = key
+    }
+}
+
+console.log({maxLetter, maxCount});
