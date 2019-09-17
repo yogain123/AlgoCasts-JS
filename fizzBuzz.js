a3c3fd614c914b5e1a7713b019b26f9ac05c572f
yogendra saxena <yogendrasaxena@Admins-MacBook-Pro-4.local>
2019-09-17 14:32:03 +0530

    Fizz Buzz Algo Added

---
+++ fizzBuzz.js
@@ -0,0 +1,14 @@
+function fizzBuzz() {
+    for (let i = 0; i <= 100; i++) {
+        if (i % 15 == 0)
+            console.log("FizzBuzz");
+        else if (i % 5)
+            console.log("Buzz");
+        else if (i % 3)
+            console.log("Fizz");
+        else
+            console.log(i);
+    }
+}
+
+fizzBuzz();
