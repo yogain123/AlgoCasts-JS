6769b30900f6696854f0ef704d4fd7d25bd284fb
yogendra saxena <yogendrasaxena@Admins-MacBook-Pro-4.local>
2019-09-17 14:28:17 +0530

    Palindrom Algo

---
+++ palindron.js
@@ -0,0 +1,14 @@
+console.log(1);
+console.log(1);
+function palindrom(a, b){
+    let c = a+b;
+    if(c>150)
+        return;
+    console.log(c);
+    a=b;
+    b=c;
+    palindrom(a,b)   // Recursion
+}
+
+palindrom(1, 1);
+
