5eac7899bddac192fb991dc7fa904c94570e1df7
yogain19 <yogendrasaxena56@gmail.com>
2019-09-20 00:12:04 +0530

    modified BS

--- bubbleSort.js
+++ bubbleSort.js
@@ -2,9 +2,10 @@ function bubbleSort(arr) {
     for (let i = 0; i < arr.length; i++) {
         for (let j = 0; j < arr.length - 1; j++) {
             if (arr[j + 1] < arr[j]) {
-                temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
+                // temp = arr[j];
+                // arr[j] = arr[j + 1];
+                // arr[j + 1] = temp;
+                [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
             }
         }
     }
