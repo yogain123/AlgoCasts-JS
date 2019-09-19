cbe53cbeba13db6d5a8d004bf6a2b2f1a39d9e4a
yogain19 <yogendrasaxena56@gmail.com>
2019-09-20 00:09:17 +0530

    Bubble Sort

---
+++ bubbleSort.js
@@ -0,0 +1,14 @@
+function bubbleSort(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length - 1; j++) {
+            if (arr[j + 1] < arr[j]) {
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+    return arr;
+}
+
+console.log(bubbleSort([4, 3, 2, 6, 7, 8]));
