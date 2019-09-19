d4280f875bbecea060313b3b4e110d57f0566298
yogain19 <yogendrasaxena56@gmail.com>
2019-09-20 00:41:25 +0530

    Selection Sort

---
+++ selectionSort.js
@@ -0,0 +1,15 @@
+function selectionSort(arr) {
+
+    for (let i = 0; i < arr.length; i++) {
+        let index = i;
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[j] < arr[index])
+                index = j
+        }
+        if (index != i)
+            [arr[i], arr[index]] = [arr[index], arr[i]];
+    }
+    return arr;
+}
+
+console.log(selectionSort([3, 2, 1, 5, 4, 3, 7, 8]));
