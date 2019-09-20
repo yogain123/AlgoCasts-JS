06f2914b89b8ac4b641c72eeb34b2267cdee868c
yogendra saxena <yogendrasaxena@Admins-MacBook-Pro-4.local>
2019-09-20 16:12:38 +0530

    Largest Consecutive Sum in Array

---
+++ LargrstConsecutiveArraySum.js
@@ -0,0 +1,22 @@
+// Java program to print largest contiguous array sum
+class LargestArraySum {
+    hola() {
+        var a = [2, -3, 4, -1, -2, 1, 5, -3];
+
+        var max_so_far = 0;
+        var max_ending_here = 0;
+
+        for (var i = 0; i < a.length; i++) {
+            max_ending_here = max_ending_here + a[i];
+            if (max_ending_here < 0)
+                max_ending_here = 0;
+            if (max_ending_here > max_so_far)
+                max_so_far = max_ending_here;
+
+        }
+        console.log(max_so_far);
+    }
+}
+
+let arr = new LargestArraySum();
+arr.hola();
