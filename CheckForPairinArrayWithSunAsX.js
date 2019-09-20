0182794203f53f4ce35097320fe8f8b09bf10f9a
yogendra saxena <yogendrasaxena@Admins-MacBook-Pro-4.local>
2019-09-20 16:05:50 +0530

    Check for Pair in Array With as X

---
+++ CheckForPairinArrayWithSunAsX.js
@@ -0,0 +1,22 @@
+function pairCheckForSum(a) {
+    var num = 16;
+    a.sort((item1, item2) => item1 - item2);
+    //display(a);
+    var first = 0;
+    var last = a.length - 1;
+    while (a[first] + a[last] != num) {
+        if (a[first] + a[last] > num)
+            last -= 1;
+        else
+            first += 1;
+    }
+
+    console.log("Numbers are  " + a[first] + " " + a[last]);
+}
+
+function display(a) {
+    for (var i = 0; i < a.length; i++)
+        console.log(a[i]);
+}
+
+pairCheckForSum([1, 4, 45, 6, 10, -8]);
