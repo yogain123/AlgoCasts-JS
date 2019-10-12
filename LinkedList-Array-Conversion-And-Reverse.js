ba74464227911b00164f0247b7738808abe52471
yogain19 <yogendrasaxena56@gmail.com>
2019-10-12 16:41:04 +0530

    LL Conversion

---
+++ LinkedList-Array-Conversion-And-Reverse.js
@@ -0,0 +1,47 @@
+/*
+ * @lc app=leetcode id=206 lang=javascript
+ *
+ * [206] Reverse Linked List
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+
+var reverseList = function(head) {
+  let arr = convertLinkedListtoArray(head);
+  arr.reverse();
+  return convertArrayToLinkedList(arr);
+};
+
+function convertLinkedListtoArray(temp) {
+  let arr = [];
+  while (temp != null) {
+    arr.push(temp.val);
+    temp = temp.next;
+  }
+  return arr;
+}
+
+function convertArrayToLinkedList(arr) {
+  if (arr.length === 0) {
+    return null;
+  }
+  let firstNode = new ListNode(arr[0]);
+  let temp = firstNode;
+  head = temp;
+  for (let i = 1; i < arr.length; i++) {
+    temp.next = new ListNode(arr[i]);
+    temp = temp.next;
+  }
+  return head;
+}
