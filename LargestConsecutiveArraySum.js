a5afd24785a41667ba4c175ce2c7e8c87d624f37
yogendra saxena <yogendrasaxena56@gmail.com>
2019-10-15 13:09:42 +0530

    Update LargestConsecutiveArraySum.js

--- LargestConsecutiveArraySum.js
+++ LargestConsecutiveArraySum.js
@@ -1,18 +1,13 @@
-class LargestArraySum {
-  getLargestArraySum() {
-    var a = [2, -3, 4, -1, -2, 1, 5, -3];
-
-    var max_so_far = 0;
-    var max_ending_here = 0;
-
-    for (var i = 0; i < a.length; i++) {
-      max_ending_here = max_ending_here + a[i];
-      if (max_ending_here < 0) max_ending_here = 0;
-      if (max_ending_here > max_so_far) max_so_far = max_ending_here;
+LargestArraySum([2, -3, 4, -1, -2, 1, 5, -3]);
+function LargestArraySum(arr){
+    let maxEnding = 0;
+    let maxSoFar = 0;
+    for (const item of arr) {
+        maxEnding+=item;
+        if(maxEnding<0)
+            maxEnding=0;
+        if(maxEnding>maxSoFar)
+            maxSoFar=maxEnding;
     }
-    console.log(max_so_far);
-  }
+    console.log(maxSoFar);
 }
-
-let largestArraySum = new LargestArraySum();
-largestArraySum.getLargestArraySum();
