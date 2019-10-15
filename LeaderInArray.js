b1c90cd34cb55efa4ba42a1bef5671ec472c85da
yogendra saxena <yogendrasaxena56@gmail.com>
2019-10-15 13:05:58 +0530

    Update LeaderInArray.js

--- LeaderInArray.js
+++ LeaderInArray.js
@@ -3,18 +3,14 @@
 Write a program to print all the LEADERS in the array. An element is leader if it is greater than all the elements to 
 its right side. And the rightmost element is always a leader. For example int the array {16, 17, 4, 3, 5, 2}, leaders are 17, 5 and 2.*/
 
-class LeaderInArray {
-  leader() {
-    var a = [16, 17, 4, 3, 5, 2];
-    var max = 0;
-    for (var i = a.length - 1; i >= 0; i--) {
-      if (a[i] >= max) {
-        max = a[i];
-        console.log(a[i]);
-      }
-    }
-  }
-}
 
-let leaderInArray = new LeaderInArray();
-leaderInArray.leader();
+leaderInArray([16, 17, 4, 3, 5, 2]);
+function leaderInArray(arr) {
+    let max = 0;
+    for (let i = arr.length-1; i >= 0; i--) {
+        if(arr[i]>max){
+        console.log(arr[i]);
+        max = arr[i];
+        }
+    }
+}
