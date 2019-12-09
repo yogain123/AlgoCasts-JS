3a5871bc2377b788a22b94a3a2e3e36465ea099c
yogendra saxena <yogendrasaxena56@gmail.com>
2019-12-09 15:55:55 +0530

    Another Easy solution for segregation of zero's and one's

--- Segregate_0s_and_1s_in_an_arrayTS.ts
+++ Segregate_0s_and_1s_in_an_arrayTS.ts
@@ -6,29 +6,49 @@ b) Keep decrementing index right while there are 1s at it
 c) If left < right then exchange arr[left] and arr[right]
 */
 
-class Segregate_0s_and_1s_in_an_array {
-  segregate(): void {
-    var a = [0, 1, 0, 1, 1, 1];
+// class Segregate_0s_and_1s_in_an_array {
+//   segregate(): void {
+//     var a = [0, 1, 0, 1, 1, 1];
 
-    var first = 0;
-    var last = a.length - 1;
+//     var first = 0;
+//     var last = a.length - 1;
 
-    while (first < last) {
-      while (a[first] != 1 && first < last) first++;
+//     while (first < last) {
+//       while (a[first] != 1 && first < last) first++;
 
-      while (a[last] != 0 && first < last) last--;
+//       while (a[last] != 0 && first < last) last--;
 
-      if (first < last) {
-        a[first] = 0;
-        a[last] = 1;
-        first++;
-        last--;
-      }
-    }
+//       if (first < last) {
+//         a[first] = 0;
+//         a[last] = 1;
+//         first++;
+//         last--;
+//       }
+//     }
 
-    console.log(a);
-  }
+//     console.log(a);
+//   }
+// }
+
+// let Seg = new Segregate_0s_and_1s_in_an_array();
+// Seg.segregate();
+
+
+//Another easy TS Solution
+seg([0, 1, 0, 1, 1, 1]);
+
+function seg(arr: number[]): void {
+    let charMap =  buildCharMap(arr);
+    console.log(charMap);
+    let zero = Array(charMap[0]).fill(0);
+    let one = Array(charMap[1]).fill(1);
+    console.log([...zero, ...one]);   // [ 0, 0, 1, 1, 1, 1 ]
 }
 
-let Seg = new Segregate_0s_and_1s_in_an_array();
-Seg.segregate();
+function buildCharMap(arr: number[]) {
+    let obj = {};
+    for (const item of arr) {
+        obj[item] = obj[item] +1 ||1;
+     }
+     return obj
+}
