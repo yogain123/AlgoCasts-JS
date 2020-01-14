de303eab8bad5a0cb4213d60694af5c29dfc0c37
yogain123 <yogendrasaxena56@gmail.com>
2020-01-14 12:58:45 +0530

    Added BST

---
+++ binary-search-tree.js
@@ -0,0 +1,55 @@
+// --- Directions
+// 1) Implement the Node class to create
+// a binary search tree.  The constructor
+// should initialize values 'data', 'left',
+// and 'right'.
+// 2) Implement the 'insert' method for the
+// Node class.  Insert should accept an argument
+// 'data', then create an insert a new node
+// at the appropriate location in the tree.
+// 3) Implement the 'contains' method for the Node
+// class.  Contains should accept a 'data' argument
+// and return the Node in the tree with the same value.
+// If the value isn't in the tree return null.
+
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+
+  insert(data) {
+    if (data < this.data && this.left) {
+      this.left.insert(data);
+    } else if (data < this.data) {
+      this.left = new Node(data);
+    } else if (data > this.data && this.right) {
+      this.right.insert(data);
+    } else if (data > this.data) {
+      this.right = new Node(data);
+    }
+  }
+
+  contains(data) {
+    if (this.data == data) {
+      return this;
+    }
+
+    if (data < this.data && this.left) {
+      return this.left.contains(data);
+    } else if (data > this.data && this.right) {
+      return this.right.contains(data);
+    } else {
+      return null;
+    }
+  }
+}
+
+let node = new Node(10);
+node.insert(5);
+node.insert(30);
+node.insert(40);
+node.insert(50);
+
+console.log(node);
