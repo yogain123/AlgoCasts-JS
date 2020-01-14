1d0e0b015387e5a1a72e3ad3496e8514b5618cca
yogain123 <yogendrasaxena56@gmail.com>
2020-01-14 13:08:29 +0530

    Validating BST

---
+++ validateBinarySearchTree.js
@@ -0,0 +1,65 @@
+// --- Directions
+// Given a node, validate the binary search tree,
+// ensuring that every node's left hand child is
+// less than the parent node's value, and that
+// every node's right hand child is greater than
+// the parent
+
+function validate(node, min = null, max = null) {
+  if (max !== null && node.data > max) {
+    return false;
+  }
+
+  if (min !== null && node.data < min) {
+    return false;
+  }
+
+  if (node.left && !validate(node.left, min, node.data)) {
+    return false;
+  }
+
+  if (node.right && !validate(node.right, node.data, max)) {
+    return false;
+  }
+
+  return true;
+}
+
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+
+  insert(data) {
+    if (data < this.data && this.left) {
+      this.left.insert(data);
+    } else if (data < this.data) {
+      this.left = new Node(data);
+    } else if (data > this.data && this.right) {
+      this.right.insert(data);
+    } else if (data > this.data) {
+      this.right = new Node(data);
+    }
+  }
+}
+
+//making a valid BST
+const n = new Node(10);
+n.insert(5);
+n.insert(15);
+n.insert(0);
+n.insert(20);
+let check = validate(n);
+console.log({ check });
+
+//making invalid BST
+const p = new Node(10);
+p.insert(5);
+p.insert(15);
+p.insert(0);
+p.insert(20);
+p.left.left.right = new Node(999);
+check = validate(p);
+console.log({ check });
