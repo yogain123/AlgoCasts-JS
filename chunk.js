7e075e88abb7532c2cc3e9df191d171ed73e3de8
yogendra saxena <yogendrasaxena56@gmail.com>
2020-02-13 23:25:13 +0530

    Update chunk.js

--- chunk.js
+++ chunk.js
@@ -9,3 +9,30 @@ function chunk(arr, size) {
   }
   return result;
 }
+
+
+//============ Other Way ================
+
+  
+console.log(chunk2([1, 2, 3,4,5,6,7,8,9], 4));
+
+function chunk2(arr, size) {
+    let res = [];
+    let first = 0;
+    let last = size;    
+    while(true){
+        res.push(arr.slice(first, last));
+        first = last;        
+        last = last + size;
+        
+        if(last>arr.length){
+            if(arr.slice(first).length===0){
+                break;
+            }
+            res.push(arr.slice(first));
+            break;
+        }
+    }
+    return res;
+
+}
