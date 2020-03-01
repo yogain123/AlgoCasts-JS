2ab50231f5504d046526011e9ae0d353374a879e
yogendra saxena <yogendrasaxena56@gmail.com>
2020-03-01 10:29:52 +0530

    Create Deep-Clone.js

---
+++ Deep-Clone.js
@@ -0,0 +1,54 @@
+//Cloning Object Deeply
+
+let obj = {
+    "name": "yogendra",
+    "age": 23,
+    "indian": true,
+    "dummy":{
+        "dummy1":{
+            "dummy2":{
+                "name":"yogendra"
+            }
+        }
+    },
+    yummy:"yummy",
+    "address": [
+      {
+        "type": "present",
+        "city": "bangalore"
+      },
+      {
+        "type": "permanent",
+        "city": "kanpur"
+      }
+    ],
+    "phno": [
+      "9840946552",
+      "1234565678"
+    ],
+    "hobbies": {
+      "type": "school",
+      "hob": "reading"
+    },
+    "location" : {
+              "lat" : 40.12,
+              "lon" : -71.34
+          }
+  }
+
+let res = {};
+
+function deepClone(obj){
+    for (const [key, value] of Object.entries(obj)) {
+        if(Object.prototype.toString.call(value) === "[object Object]"){
+            res[key] = value
+            deepClone(value);
+        }else{
+            res[key] = value
+        }
+    }
+    return res;
+}
+
+let result = deepClone(obj)
+console.log(JSON.stringify(result));
