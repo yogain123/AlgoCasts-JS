0da9300a6ecacabfe74320d46ac456fc9262f53e
yogendra saxena <yogendrasaxena56@gmail.com>
2020-03-06 14:39:58 +0530

    Create Roman-To-Integer.js

---
+++ leetcode/Roman-To-Integer.js
@@ -0,0 +1,33 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+
+const romanMapping = {
+    I:1,
+    V:5,
+    X:10,
+    L:50,
+    C:100,
+    D:500,
+    M:1000
+}
+var romanToInt = function(s) {
+    let sum = 0;
+    let arrString = [...s];
+    let arrNumber = arrString.map(item=>romanMapping[item]);
+    for(let i=0;i<arrNumber.length;){
+        if(!arrNumber[i+1]){
+            sum = sum + arrNumber[i];
+            break;
+        }
+        if(arrNumber[i]<arrNumber[i+1]){
+            sum = sum + arrNumber[i+1] - arrNumber[i];
+            i=i+2;
+        }else{
+            sum = sum + arrNumber[i];
+            i++;
+        }
+    }
+    return sum;
+};
