6ba1dbca2a36f98490018a16a32afb2293aa6593
yogain123 <yogendrasaxena56@gmail.com>
2020-03-09 12:49:45 +0530

    Added Locale Format Algo

---
+++ LocaleFormat.js
@@ -0,0 +1,38 @@
+function localeformat(str) {
+  if (+str <= 999) {
+    return str;
+  } else {
+    let arr = [...str];
+    let lastPart = arr.slice(-3);
+    let firstPart = arr.slice(0, arr.length - 3);
+    console.log({ firstPart, lastPart });
+    resultStr = "";
+    let removeComma = false;
+    if (firstPart.length % 2 === 0) {
+      removeComma = true;
+      for (let i = firstPart.length - 1; i >= 0; i--) {
+        if (i % 2 === 1) {
+          resultStr = firstPart[i] + resultStr;
+        } else {
+          resultStr = "," + firstPart[i] + resultStr;
+        }
+      }
+    } else {
+      for (let i = firstPart.length - 1; i >= 0; i--) {
+        if (i % 2 === 0) {
+          resultStr = firstPart[i] + resultStr;
+        } else {
+          resultStr = "," + firstPart[i] + resultStr;
+        }
+      }
+    }
+    resultStr = resultStr + "," + lastPart.join("");
+    if (removeComma) {
+      resultStr = resultStr.substring(1);
+    }
+    return resultStr;
+  }
+}
+
+let result = localeformat("100000000000000");
+console.log(result); // 10,00,00,00,00,00,000
