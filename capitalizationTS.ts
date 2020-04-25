166bc41a7730e4d42fc4535216f95a4d1036d980
Anurag Hazra <hazru.anurag@gmail.com>
2020-04-25 13:51:38 +0530

    feat: added ts version of capitalization2 method

--- capitalizationTS.ts
+++ capitalizationTS.ts
@@ -1,4 +1,5 @@
 console.log(capitalization("hello my name is yogendra")); // Hello My Name Is Yogendra
+console.log(capitalization2("hello my name is yogendra")); // Hello My Name Is Yogendra
 
 function capitalization(sentence: string): string {
   let temp = sentence.split(" ");
@@ -7,4 +8,9 @@ function capitalization(sentence: string): string {
     return res;
   });
   return modifiedTemp.join(" ");
+}
+
+function capitalization2(str: string): string {
+  str.match(/\s\w|^\w/mig).forEach(m => str = str.replace(m, m.toUpperCase()))
+  return str;
 }
